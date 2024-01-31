b0d037ac7c6d7b0d42e2f16c9ee70a6910cea55b
t4sk <tsk.nakamura@gmail.com>
2024-01-31 09:19:51 +0900

    exercise comment

--- foundry/test/uniswap-v2/exercises/UniswapV2Swap.test.sol
+++ foundry/test/uniswap-v2/exercises/UniswapV2Swap.test.sol
@@ -38,6 +38,9 @@ contract UniswapV2SwapTest is Test {
         uint amountIn = 1e18;
         uint amountOutMin = 1;
 
+        // Write your code here
+        // Don’t change any other code
+
         assertGe(mkr.balanceOf(user), amountOutMin, "MKR balance of user");
     }
 
@@ -51,6 +54,9 @@ contract UniswapV2SwapTest is Test {
 
         uint amountOut = 0.1 * 1e18;
         uint amountInMax = 1e18;
+
+        // Write your code here
+        // Don’t change any other code
 
         assertEq(mkr.balanceOf(user), amountOut, "MKR balance of user");
     }
