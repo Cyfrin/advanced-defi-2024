2359a22ea40f561815736ea7201e51a132830d07
t4sk <tsk.nakamura@gmail.com>
2024-01-31 09:15:00 +0900

    code fmt

--- foundry/test/uniswap-v2/solutions/UniswapV2Swap.test.sol
+++ foundry/test/uniswap-v2/solutions/UniswapV2Swap.test.sol
@@ -35,8 +35,8 @@ contract UniswapV2SwapTest is Test {
         path[1] = DAI;
         path[2] = MKR;
 
-        uint amountIn = 1e18;
-        uint amountOutMin = 1;
+        uint256 amountIn = 1e18;
+        uint256 amountOutMin = 1;
 
         vm.prank(user);
         // Input token amount and all subsequent output token amounts
@@ -63,8 +63,8 @@ contract UniswapV2SwapTest is Test {
         path[1] = DAI;
         path[2] = MKR;
 
-        uint amountOut = 0.1 * 1e18;
-        uint amountInMax = 1e18;
+        uint256 amountOut = 0.1 * 1e18;
+        uint256 amountInMax = 1e18;
 
         vm.prank(user);
         // Input token amount and all subsequent output token amounts
